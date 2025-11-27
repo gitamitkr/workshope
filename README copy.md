c8ffce53506114933ebc34c5912fcfd143f37262
gitamitkr <amitkumardot0123@gmail.com>
2025-11-27 13:59:22 +0530

    commit

---
+++ README copy.md
@@ -0,0 +1,2 @@
+# workshope
+This project is created in the tranning in the college
---
+++ models.py
@@ -0,0 +1,59 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db=SQLAlchemy()
+
+class Admin(db.Model):
+    __tablename__ = 'admins'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120),unique=True,nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f"Admin {self.username}>"
+
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120),unique=True,nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    applications=db.relationship('Application', backref='student',lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Application(db.Model):
+    __tablename__ = 'application'
+    id = db.Column(db.Integer, primary_key=True)
+    student_id =db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    resume_filename=db.Column(db.String(255),nullable=False)
+    status = db.Column(db.String(50),default='Pending')
+    created_at=db.Column(db.DateTime,default=datetime.utcnow )
+    approved_at=db.Column(db.DateTime,nullable=True)
+
+       
+    
+class Item(db.Model):
+    __tablename__="items"
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(120),nullable=False)
+    description=db.Column(db.Text,nullable=True)
+    created_at=db.Column(db.DateTime,default=datetime.utcnow )
+
+
+    def __repr__(self):
+        return f"<Item{self.id} {self.title}>"
+    
